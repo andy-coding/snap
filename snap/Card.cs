344814ed571e2ce029f5c20ca098f9111cde0c8b
andy-coding <32386192+andy-coding@users.noreply.github.com>
2017-09-29 12:37:52 +0100

    Add project files.

---
+++ snap/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snap
+{
+    class Card
+    {
+
+        enum cardnum { Ace = 13, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, J = 10, Q = 11, K = 12 };
+
+        enum suit { C, D, H, S };
+
+
+
+
+        public void CreatePack()
+        {
+
+        }
+
+
+
+
+
+
+        public static void DrawCard(int x, string y)
+        {
+            Console.WriteLine("|----------|");
+            Console.WriteLine("|          |");
+            Console.WriteLine("| {0}        |", x);
+            Console.WriteLine("|          |");
+            Console.WriteLine("|    {0}     |", y);
+            Console.WriteLine("|          |");
+            Console.WriteLine("|       {0}  |", x);
+            Console.WriteLine("|          |");
+            Console.WriteLine("|----------|");
+        }
+    }
+}
+
+
+
+
---
+++ snap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace snap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card.DrawCard(2, "H");
+
+        }
+
+    }
+
+
+}
